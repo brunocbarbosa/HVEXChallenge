95f5b81aa9bb91a86adaa1912ee32d50ba910bf9
Bruno Barbosa <bruno.cbarbosa90@gmail.com>
2020-02-10 13:01:54 -0300

    first commit

---
+++ api/config/express.js
@@ -0,0 +1,19 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    consign = require('consign');
+
+/* CONFIGURE EXPRESS */
+const app = express();
+
+
+/* CONFIGURE BODY-PARSER */
+app.use(bodyParser.urlencoded({ extended : true }))
+app.use(bodyParser.json());
+
+/* CONFIGURE CONSIGN */
+consign()
+    .include('app/routes')
+    .then('app/controllers')
+    .into(app)
+
+module.exports = app;
---
+++ api/server.js
@@ -0,0 +1,7 @@
+const app = require('./config/express');
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+    console.log(`Server ON in port ${port}`)
+});
